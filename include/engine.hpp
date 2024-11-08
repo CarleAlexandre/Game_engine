78407ff8673ae24b1014052f3151c93deade2c2b
fatmeat <pathofyggdrasil@gmail.com>
2024-11-08 08:06:28 +0100

    aewr

--- include/engine.hpp
+++ include/engine.hpp
@@ -1,10 +1,37 @@
 #ifndef ENGINE_HPP
 # define ENGINE_HPP
 
-typedef struct s_item{
+#include <vector>
+
+typedef struct s_point {
+	float x, y;
+}	point_t ;
+
+typedef struct s_player {
+	point_t pos;
+}	player_t;
+
+typedef struct s_object {
+	point_t pos;
+}	object_t;
+
+typedef struct s_item {
+	point_t pos;
 }	item_t;
 
-typedef struct s_entity{
+typedef struct s_entity {
+	point_t pos;
 }	entity_t;
+
+typedef struct s_wall {
+	point_t pos;
+}	wall_t;
+
+typedef struct s_scene {
+	std::vector<player_t> players;
+	std::vector<object_t> objects;
+	std::vector<entity_t> entities;
+	std::vector<wall_t> walls;
+}	scene_t;
 
 #endif
--- source/render.cpp
+++ source/render.cpp
@@ -1,4 +1,5 @@
 #include <raylib.h>
+#include <engine.hpp>
 
 void light_culling() {
 
@@ -20,7 +21,21 @@ void update_light() {
 
 }
 
-void render() {
-	//create
-	//
+void render(scene_t scene, RenderTexture2D fbo, Camera2D camera, Shader posprocess_shader, Shader lightShader) {
+	BeginTextureMode(fbo);
+		ClearBackground(BLACK);
+		BeginMode2D(camera);
+			for (auto span : scene.walls);
+			for (auto span : scene.objects);
+			for (auto span : scene.entities);
+			for (auto span : scene.players);
+		EndMode2D();
+	EndTextureMode();
+
+	BeginDrawing();
+		ClearBackground(BLACK);
+		BeginShaderMode(posprocess_shader);
+			DrawTextureRec(fbo.texture, {0, 0, (float)GetScreenWidth(), (float)-GetScreenHeight()}, {0, 0}, WHITE);
+		EndShaderMode();
+	EndDrawing();
 }
