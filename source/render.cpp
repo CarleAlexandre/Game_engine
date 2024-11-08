8eeae1160b84b9f2c0202e748e6b96c59f234352
XORDia <pathofyggdrasil@gmail.com>
2024-11-08 05:18:04 +0100

    UwU

---
+++ source/render.cpp
@@ -0,0 +1,26 @@
+#include <raylib.h>
+
+void light_culling() {
+
+}
+
+void entity_culling() {
+
+}
+
+void object_culling() {
+
+}
+
+void walls_culling() {
+	
+}
+
+void update_light() {
+
+}
+
+void render() {
+	//create
+	//
+}
