4fd53b664acf9e4e94befd933b55c34920dfabcc
fatmeat <pathofyggdrasil@gmail.com>
2024-11-08 21:51:22 +0100

    areok

--- include/engine.hpp
+++ include/engine.hpp
@@ -13,6 +13,7 @@ typedef struct s_player {
 
 typedef struct s_object {
 	point_t pos;
+	float rad;
 }	object_t;
 
 typedef struct s_item {
@@ -21,17 +22,34 @@ typedef struct s_item {
 
 typedef struct s_entity {
 	point_t pos;
+	int size;
 }	entity_t;
 
 typedef struct s_wall {
-	point_t pos;
+	point_t p1;
+	point_t p2;
 }	wall_t;
 
-typedef struct s_scene {
-	std::vector<player_t> players;
-	std::vector<object_t> objects;
-	std::vector<entity_t> entities;
+typedef struct s_light {
+	point_t pos;
+	int type;
+	int intensity;
+	int pos_loc;
+	int type_loc;
+	int intensity_loc;
+}	light_t;
+
+typedef struct s_map {
+	std::vector<object_t> objs;
+	std::vector<entity_t> entity;
 	std::vector<wall_t> walls;
+}	map_t;
+
+typedef struct s_scene {
+	std::vector<player_t*> players;
+	std::vector<object_t*> objects;
+	std::vector<entity_t*> entities;
+	std::vector<wall_t*> walls;
 }	scene_t;
 
 #endif
--- source/render.cpp
+++ source/render.cpp
@@ -1,8 +1,27 @@
 #include <raylib.h>
 #include <engine.hpp>
 
-void light_culling() {
+void create_light(std::vector<light_t> lights, point_t pos, int type, int intensity, Shader light_shader) {
+	light_t light = {0};
 
+	light.intensity = intensity;
+	light.pos = pos;
+	light.type = type;
+
+	light.pos_loc = GetShaderLocation(light_shader, "light[%i].pos");
+	light.type_loc = GetShaderLocation(light_shader, "light[%i].type");
+	light.intensity_loc = GetShaderLocation(light_shader, "light[%i].intensity");
+
+	lights.push_back(light);
+	update_light(lights, light_shader);
+}
+
+void update_light(std::vector<light_t> lights, Shader light_shader) {
+	for (auto light: lights) {
+		SetShaderValue(light_shader, light.pos_loc, &light.pos, SHADER_UNIFORM_VEC2);
+		SetShaderValue(light_shader, light.type_loc, &light.type, SHADER_UNIFORM_INT);
+		SetShaderValue(light_shader, light.intensity_loc, &light.intensity, SHADER_UNIFORM_INT);
+	}
 }
 
 void entity_culling() {
@@ -13,22 +32,29 @@ void object_culling() {
 
 }
 
-void walls_culling() {
+void walls_culling(scene_t scene, Camera2D camera) {
 	
 }
 
-void update_light() {
+void render(map_t map, RenderTexture2D fbo, Camera2D camera, Shader posprocess_shader, Shader light_shader) {
+	scene_t scene;
+	walls_culling(scene, camera);
 
-}
-
-void render(scene_t scene, RenderTexture2D fbo, Camera2D camera, Shader posprocess_shader, Shader lightShader) {
 	BeginTextureMode(fbo);
 		ClearBackground(BLACK);
 		BeginMode2D(camera);
-			for (auto span : scene.walls);
-			for (auto span : scene.objects);
-			for (auto span : scene.entities);
-			for (auto span : scene.players);
+			for (auto span : scene.walls) {
+				DrawLine(span->p1.x, span->p2.x, span->p1.y, span->p2.y, WHITE);
+			}
+			for (auto span : scene.objects) {
+				DrawCircle(span->pos.x, span->pos.y, span->rad, GREEN);
+			}
+			for (auto span : scene.entities) {
+				DrawCircle(span->pos.x, span->pos.y, span->size, RED);
+			}
+			for (auto span : scene.players) {
+				DrawCircle(span->pos.x, span->pos.y, 5, BLUE);
+			}
 		EndMode2D();
 	EndTextureMode();
 
@@ -39,3 +65,5 @@ void render(scene_t scene, RenderTexture2D fbo, Camera2D camera, Shader posproce
 		EndShaderMode();
 	EndDrawing();
 }
+
+
