3f569f92dc49e2220811afe0e661d8a93c2bfcb6
fatmeat <pathofyggdrasil@gmail.com>
2024-11-12 21:52:52 +0100

    afds

--- source/physic.cpp
+++ source/physic.cpp
@@ -5,3 +5,9 @@ Vector3 apply_gravity(Vector3 pos, float mass) {
 	Vector3 g = {1, mass * 0.2, 1};
 	return (Vector3CrossProduct(g, pos));
 }
+
+Vector3 apply_drag(Vector3 pos, float mass) {
+	Vector3 drag = {1 / mass, 1 / mass, 1 / mass};
+	return (Vector3CrossProduct(pos, drag));
+}
+
