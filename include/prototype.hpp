bc6dab0a03ff47dc3b7f0d15a8a6781eb88eade7
fatmeat <pathofyggdrasil@gmail.com>
2024-11-14 02:09:57 +0100

    UwU

--- include/prototype.hpp
+++ include/prototype.hpp
@@ -3,10 +3,10 @@
 
 #include <engine.hpp>
 engine_t init_engine(void);
+void close_engine(engine_t &engine);
 
 void update_light(std::vector<light_t> &lights, Shader light_shader);
 void create_light(std::vector<light_t> &lights, Vector3 pos, int type, int intensity, Shader light_shader);
 void render(level_t level, engine_t &engine, void (*render_ui)(void));
-void close_engine(engine_t &engine);
 
 #endif
