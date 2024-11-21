f60212504cf7d167c09813358b66696e4a9b15d7
fatmeat <pathofyggdrasil@gmail.com>
2024-11-21 20:19:58 +0100

    asdf

---
+++ include/sparse_voxel_octree.hpp
@@ -0,0 +1,18 @@
+struct Node {
+	bool isleaf;
+	Node *children[8];
+	void *data;
+};
+
+class SparseVoxelOctree {
+	private:
+
+	public:
+
+	SparseVoxelOctree() {
+
+	}
+	~SparseVoxelOctree() {
+
+	}
+};
