d09da1caccebefc3e849f7f6106788c992e286f3
fatmeat <pathofyggdrasil@gmail.com>
2024-12-05 00:18:03 +0100

    asdf

--- source/networking.cpp
+++ source/networking.cpp
@@ -2,7 +2,3 @@
 #include <enet.h>
 #include <librg.h>
 #include <json.h>
-
-int main(int ac, char **av, char **envp) {
-	
-}
--- source/voxel.cpp
+++ source/voxel.cpp
@@ -1,14 +1,33 @@
 #include <engine.hpp>
+#include <vector>
 
-void lod_voxel() {
+#define GRAY_VALUE(c) ((float)(c.r + c.g + c.b)/3.0f)
+
+void lod_voxel(Vector3 pos) {
+
+}
+
+void heightmap_voxel(Image heightmap) {
+	const int mapX = heightmap.width;
+	const int mapZ = heightmap.height;
+
+	Color *pixels = LoadImageColors(heightmap);
+	std::vector<std::vector<int>> height = std::vector<std::vector<int>>(mapZ);
+	for (auto tmp : height) {
+		tmp = std::vector<int>(mapX);
+	}
+
+	for (int z = 0; z < mapZ - 1; z++) {
+		for (int x = 0; x < mapX - 1; x++) {
+			height[z][x] = GRAY_VALUE(pixels[x + (z + 1)*mapX]);
+		}
+	}
+
 	
 }
 
-void heightmap_voxel() {
-
-}
-
 void add_map_obj() {
+	 
 }
 
 void raycaster(Camera3D camera, level_t level) {
