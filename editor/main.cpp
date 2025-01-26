7fb737705f6511c34c8fea6b6729bafb150347dc
fatmeat <pathofyggdrasil@gmail.com>
2025-01-26 19:45:45 +0100

    wqe

--- editor/main.cpp
+++ editor/main.cpp
@@ -1,17 +1,54 @@
 
-#include <raylib.h>
-#include <raymath.h>
+#include <engine.hpp>
 #define RAYGUI_IMPLEMENTATION
 #include <raygui.h>
 
-int current_level = 0;
-int tool = 0;
-bool saved = false;
+int	tool = 0;
+bool	saved = false;
+int	editor_mode = 0;
 
 void	startscreen() {
 	BeginDrawing();
 	EndDrawing();
 }
+
+// ui
+
+void	top_bar() {
+	bool mode_enable = false;
+	
+	GuiStatusBar({0, 0, (float)GetScreenWidth(), 30}, "");
+	if (GuiDropdownBox({0, 0, 120, 30}, "map;item;obj;ui", &editor_mode, mode_enable)) {
+		mode_enable != mode_enable;
+	}
+}
+
+void	left_bar() {
+	GuiDummyRec({0, 30, 100, (float)GetScreenHeight()}, "Debug");
+}
+
+void	right_bar() {
+	GuiDummyRec({(float)GetScreenWidth() - 100, 30, 100, (float)GetScreenHeight()}, "setting");
+}
+
+//editor mode
+
+void	map_editor() {
+}
+
+void	item_editor() {
+
+}
+
+void	obj_editor() {
+
+}
+
+void	ui_editor() {
+
+}
+
+//
 
 int	main(void) {
 	InitWindow(1820, 980, "UwUditor");
@@ -24,10 +61,40 @@ int	main(void) {
 		.zoom = 0.8,
 	};
 
-	GuiLoadStyle("include/style_cyber.rgs");
+	GuiLoadStyle("include/style_terminal.rgs");
 
 	fbo = LoadRenderTexture(1590, 960);
 	while (!WindowShouldClose()) {
+
+		BeginDrawing();
+
+			top_bar();
+			left_bar();
+			right_bar();
+
+			BeginScissorMode(100, 30, GetScreenWidth() - 200, GetScreenHeight() - 30);
+			switch (editor_mode) {
+				case (0): {
+					map_editor();
+					break;
+				}
+				case (2): {
+					item_editor();
+					break;
+				}
+				case (3): {
+					obj_editor();
+					break;
+				}
+				case (1): {
+					ui_editor();
+					break;
+				}
+				default:break;
+			}
+			EndScissorMode();
+
+		EndDrawing();
 	}
 
 	CloseWindow();
