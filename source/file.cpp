e7c7207d222bd44be3d82d469ed1418e7e057ad0
fatmeat <pathofyggdrasil@gmail.com>
2025-01-29 06:03:04 +0100

    asdf

--- source/file.cpp
+++ source/file.cpp
@@ -1,10 +1,13 @@
 #include <engine.hpp>
 
-int check_file(char *magic, const char *filename) {
-	int ret = 0;
+int	check_file(char *magic, const char *filename) {
 	if (!FileExists(filename))
-		ret = -1;
-	return (ret);
+		return (-1);
+	int filesize = 0;
+	unsigned char *data = LoadFileData(filename, &filesize);
+
+	
+	return (0);
 }
 
 // typedef struct s_item {
@@ -25,7 +28,7 @@ item_t	item_format(const char *filename) {
 }
 
 char	*format_item(item_t item) {
-
+	
 }
 
 void	save_item() {
