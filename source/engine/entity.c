d52ca07907c1103b9d4bc25ea13a6eeabe0d09a0
carle <pathofyggdrasil@gmail.com>
2025-04-07 00:40:07 +0200

    working on enitty

--- source/engine/entity.c
+++ source/engine/entity.c
@@ -3,25 +3,54 @@
 entity_pool	pool;
 
 void	entity_pool_init(void) {
-	pool.info = calloc(MAX_ENTITY, sizeof(entity_info));
-	assert(pool.info);
-	pool.stats = calloc(MAX_ENTITY, sizeof(entity_stats));
-	assert(pool.stats);
+	// entity_stats
+	pool.health = calloc(MAX_ENTITY, sizeof(int32_t));
+	pool.max_health = calloc(MAX_ENTITY, sizeof(uint32_t));
+	pool.regeneration = calloc(MAX_ENTITY, sizeof(uint16_t));
+	pool.damage = calloc(MAX_ENTITY, sizeof(uint16_t));
+	// entity_info
+	pool.position = calloc(MAX_ENTITY, sizeof(Vector3));
+	pool.min = calloc(MAX_ENTITY, sizeof(Vector3));
+	pool.max = calloc(MAX_ENTITY, sizeof(Vector3));
+	pool.direction = calloc(MAX_ENTITY, sizeof(Vector3));
+	pool.velocity = calloc(MAX_ENTITY, sizeof(Vector3));
+
+	assert(pool.health);
+	assert(pool.max_health);
+	assert(pool.regeneration);
+	assert(pool.damage);
+	assert(pool.position);
+	assert(pool.min);
+	assert(pool.max);
+	assert(pool.direction);
+	assert(pool.velocity);
+
+	pool.capacity = MAX_ENTITY;
+	pool.size = 0;
+	memset(pool.controle_bit, 0, sizeof(pool.controle_bit));
 }
 
 void	entity_pool_destroy(void) {
-	free(pool.info);
-	free(pool.stats);
-}
-
-void	entity_spawn() {
-
-}
-
-void	entity_clear() {
-	
+	// entity_stats
+	free(pool.health);
+	free(pool.max_health);
+	free(pool.regeneration);
+	free(pool.damage);
+	// entity_info
+	free(pool.position);
+	free(pool.min);
+	free(pool.max);
+	free(pool.direction);
+	free(pool.velocity);
 }
 
 void	entity_pool_update() {
+}
 
+void	entity_spawn(entity nwaps) {
+	
+}
+
+void	entity_kill() {
+	
 }
--- source/engine/entity.h
+++ source/engine/entity.h
@@ -5,33 +5,41 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 #include <raylib.h>
 
-#define MAX_ENTITY 4096
-
-typedef struct entity_stats {
-	int		health;
-	uint32_t	max_health;
-	unsigned short 	regeneration;
-	unsigned short	damage;
-}	entity_stats;
-
-typedef struct	entity_info {
-	Vector3	position;
-	Vector3	min;
-	Vector3	max;
-	Vector3	direction;
-	Vector3	velocity;
-}	entity_info;
+#define MAX_ENTITY 100000000 //100 000 000
 
 typedef struct entity_pool {
-	entity_stats	*stats;
-	entity_info	*info;
+	// entity_stats
+	int32_t		*health;
+	uint32_t	*max_health;
+	uint16_t 	*regeneration;
+	uint16_t	*damage;
+	// entity_info
+	Vector3		*position;
+	Vector3		*min;
+	Vector3		*max;
+	Vector3		*direction;
+	Vector3		*velocity;
+	// data control
+	uint64_t	controle_bit[MAX_ENTITY / sizeof(uint64_t)];
+	uint32_t	size;
+	uint32_t	capacity;
 }	entity_pool;
 
 typedef struct	entity {
-	entity_stats	*stats;
-	entity_info	*info;
+	// entity_stats
+	int32_t		health;
+	uint32_t	max_health;
+	uint16_t 	regeneration;
+	uint16_t	damage;
+	// entity_info
+	Vector3		position;
+	Vector3		min;
+	Vector3		max;
+	Vector3		direction;
+	Vector3		velocity;
 }	entity;
 
 #endif
